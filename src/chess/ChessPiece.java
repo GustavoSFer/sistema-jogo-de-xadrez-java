2d4d8aa0b719fb1dbba580a4823b7f8ac5373969
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-12 21:44:44 -0300

    Feat: Criando o enum sobre as cores da peça e a peça do xadrez

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package chess;
+
+import boardgame.Piece;
+import boardgame.Position;
+
+public class ChessPiece extends Piece {
+  private Color color;
+
+  public ChessPiece(Position position, Color color) {
+    super(position);
+    this.color = color;
+  }
+
+  public Color getColor() {
+    return color;
+  }
+
+}
